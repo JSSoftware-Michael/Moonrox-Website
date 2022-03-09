0de92c3501cd16d20360d7bfd83bc93b813893ac
work134 <ms.live104@gmail.com>
2022-03-09 00:02:23 -0600

    Got the header done, I think

--- moonrox/src/App.js
+++ moonrox/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import contact from './img/contact.svg';
+import searchIcon from './img/search.svg';
+import logo from './img/logo.svg';
+import logo_glow from './img/logo_glow.svg';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div className="App">
+
+        <div className="header">
+
+          <div className="searchInput">
+            <input className="search" type="text" id="search"></input>
+              <div className='center noSelect'>
+                <img src={searchIcon} className="searchIcon"></img>
+                <p class="searchText">Search</p>
+              </div>
+          </div>
+
+          <div className="logoContainer">
+            <img src={logo_glow} className="logo noSelect center loopBlur"></img>
+            <img src={logo_glow} className="logo noSelect center loopBlur"></img>
+            <img src={logo} className="logo noSelect center"></img>
+          </div>
+
+            <div className="contactButton contactBorderGradient">
+              <div className='center'>
+                <img src={contact} className="contactIcon"></img>
+                <p className="contactText">Contact</p>
+              </div>
+            </div>
+          </div>
+        </div>
+    </body>
   );
 }
 
 export default App;
+
